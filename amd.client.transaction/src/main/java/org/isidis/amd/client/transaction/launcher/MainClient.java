02c55ca9726116a7e814ec99b1ae6e8dfa2ffe4c
steventen <steven@enten.fr>
2013-12-18 15:47:57 +0100

    Add pauses in the client

--- amd.client.transaction/src/main/java/org/isidis/amd/client/transaction/launcher/MainClient.java
+++ amd.client.transaction/src/main/java/org/isidis/amd/client/transaction/launcher/MainClient.java
@@ -1,6 +1,8 @@
 package org.isidis.amd.client.transaction.launcher;
 
+import java.io.IOException;
 import java.rmi.RMISecurityManager;
+import java.util.Scanner;
 
 import org.isidis.amd.cos.transactions.Transaction;
 import org.isidis.amd.cos.transactions.api.TransactionAPI;
@@ -26,6 +28,14 @@ public class MainClient
 	public static final String TRAININGCAMP_RESOURCE_ADDRESS = String.format("rmi://%s:%s/%s/", SERVICES_HOST, TRAININGCAMP_RESOURCE_PORT, TRAININGCAMP_RESOURCE_SERVICE);
 	public static final String BATTLEFIELD_RESOURCE_ADDRESS = String.format("rmi://%s:%s/%s/", SERVICES_HOST, BATTLEFIELD_RESOURCE_PORT, BATTLEFIELD_RESOURCE_SERVICE);
 	
+	public static void pause(String pMessage) {
+		System.out.print(pMessage);
+		pause();
+	}
+	public static void pause() {
+		new Scanner(System.in).nextLine();
+	}
+	
 	public static void main(String[] args) 
 	{
 		if (System.getSecurityManager() == null) 
@@ -36,26 +46,46 @@ public class MainClient
 		
 		try 
 		{	
-			TransactionAPI api = TransactionAPI.init();	
+			pause(">> Initialisation of the Transaction API");
+			TransactionAPI api = TransactionAPI.init();
+			pause(">> Registration of the Base resource");
 			Base base = api.registerAsResource(Base.class, BASE_RESOURCE_ADDRESS);
+			pause(">> Registration of the TraningCamp resource");
 			TrainingCamp trainingcamp = api.registerAsResource(TrainingCamp.class, TRAININGCAMP_RESOURCE_ADDRESS);
+			pause(">> Registration of the BattleField resource");
 			BattleField battlefield = api.registerAsResource(BattleField.class, BATTLEFIELD_RESOURCE_ADDRESS);
+			pause(">> Creation of the first transaction T1");
 			Transaction T1 = api.createTransaction();
+			pause(">> Attach the resource Base on T1");
 			api.attachResource(T1, base);
+			pause(">> Creation of the second transaction T2");
 			Transaction T2 = api.createTransaction(base, trainingcamp);
+			pause(">> Creation of the transaction T3");
 			Transaction T3 = api.createTransaction(battlefield);
+			pause(">> Attach the resource TrainingCamp on T1");
 			api.attachResource(T3,  trainingcamp);
+			pause(">> Begin T1");
 			T1.begin();
+			pause(">> Begin T2");
 			T2.begin();
+			pause(">> Calls of the resources");
 			battlefield.deploy(trainingcamp.train(base.getNovice("JD")));
+			pause(">> Begin T3");
 			T3.begin();
+			pause(">> Calls of the resources");
 			battlefield.deploy(trainingcamp.train(base.getNovice("Bond")));
+			pause(">> Rollback T2");
 			T2.rollback();
+			pause(">> Calls of the resources");
 			battlefield.deploy(trainingcamp.train(base.getNovice("De Gaulle")));
+			pause(">> Commit T1");
 			T1.commit();
+			pause(">> Begin T2");
 			T2.begin();
 			battlefield.deploy(trainingcamp.train(base.getNovice("Bond")));
+			pause(">> Rollback T2");
 			T2.rollback();
+			pause(">> Resume of the transaction T3");
 			System.out.println(T3.getInvocationsResume());
 		} 
 		catch (Exception e) 
