9edb4ee0cecf5b1eaf21d4333606b38dc5fbfefb
steventen <steven@enten.fr>
2013-12-19 16:57:04 +0100

    Javadocumented of the amd.resources.{base,battlefield,trainingcamp} projects

--- amd.resources.battlefield/src/main/java/org/isidis/amd/resources/battlefield/BattleFieldBean.java
+++ amd.resources.battlefield/src/main/java/org/isidis/amd/resources/battlefield/BattleFieldBean.java
@@ -10,18 +10,37 @@ import org.isidis.amd.cos.transactions.TransactionException;
 import org.isidis.amd.resources.common.BattleField;
 import org.isidis.amd.resources.common.Unit;
 
+/**
+ * Concrete battlefield. It's an implementation of the interface BattleField.
+ * @see BattleField
+ */
 public class BattleFieldBean extends UnicastRemoteObject implements BattleField 
 {
 	private static final long serialVersionUID = -6622439355462427448L;
+	/**
+	 * Register of the battlefield.
+	 */
 	private List<Unit> register;
+	/**
+	 * Transaction that will be used by the transaction operations (prepare, commit, rollback).
+	 */
 	private Transaction currentTransaction;
 	
+	/**
+	 * Constructor of a new battlefield resource.
+	 * @throws RemoteException
+	 */
 	public BattleFieldBean() throws RemoteException 
 	{
 		super();
 		register = new ArrayList<Unit>();
 	}
-	
+	/**
+	 * Returns a warrior unit.
+	 * @param pUnit The soldier unit who is deployed on the battlefield.
+	 * @return A warrior unit.
+	 * @throws RemoteException
+	 */
 	public Unit deploy(Unit pUnit) throws RemoteException 
 	{
 		Unit novice = new Warrior(pUnit);
@@ -29,27 +48,40 @@ public class BattleFieldBean extends UnicastRemoteObject implements BattleField
 		System.out.println("Register updated");
 		return novice;
 	}
-	
+	/**
+	 * Prepare the transaction resource with the current transaction (activated by the activeTransaction method).
+	 * @throws RemoteException
+	 */
 	@Override
 	public void prepare() throws RemoteException 
 	{
 		System.out.println("Base resource is now prepared by the transaction "+ currentTransaction.getId());
 	}
-	
+	/**
+	 * Commit the transaction resource with the current transaction (activated by the activeTransaction method).
+	 * @throws RemoteException
+	 */
 	@Override
 	public void commit() throws TransactionException, RemoteException 
 	{
 		System.out.println(String.format("Base resource is now committed (%d operation%s) by the transaction %d ", currentTransaction.getInvocations().size()
 				, (currentTransaction.getInvocations().size() == 1)?"":"s", currentTransaction.getId()));
 	}
-	
+	/**
+	 * Rollback the transaction resource with the current transaction (activated by the activeTransaction method).
+	 * @throws RemoteException
+	 */
 	@Override
 	public void rollback() throws TransactionException, RemoteException 
 	{
 		System.out.println(String.format("Base resource is now rollback (%d operation%s) by the transaction %d ", currentTransaction.getInvocations().size()
 				, (currentTransaction.getInvocations().size() == 1)?"":"s", currentTransaction.getId()));
 	}
-
+	/**
+	 * Active a transaction that will be used by the transaction operations.
+	 * @param pTransaction The transaction that will be used by the transaction operations (prepare, commit, rollback).
+	 * @throws RemoteException
+	 */
 	@Override
 	public void activeTransaction(Transaction pTransaction) throws RemoteException
 	{
--- amd.resources/amd.resources.base/src/main/java/org/isidis/amd/resources/base/BaseBean.java
+++ amd.resources/amd.resources.base/src/main/java/org/isidis/amd/resources/base/BaseBean.java
@@ -10,18 +10,37 @@ import org.isidis.amd.cos.transactions.TransactionException;
 import org.isidis.amd.resources.common.Base;
 import org.isidis.amd.resources.common.Unit;
 
+/**
+ * Concrete base. It's an implementation of the interface Base.
+ * @see Base
+ */
 public class BaseBean extends UnicastRemoteObject implements Base 
 {
 	private static final long serialVersionUID = -6622439355462427448L;
+	/**
+	 * Register of the base.
+	 */
 	private List<Unit> register;
+	/**
+	 * Transaction that will be used by the transaction operations (prepare, commit, rollback).
+	 */
 	private Transaction currentTransaction;
 	
+	/**
+	 * Constructor of a new base resource.
+	 * @throws RemoteException
+	 */
 	public BaseBean() throws RemoteException 
 	{
 		super();
 		register = new ArrayList<Unit>();
 	}
-	
+	/**
+	 * Returns a novice unit.
+	 * @param pName Name of the novice.
+	 * @return A novice unit.
+	 * @throws RemoteException
+	 */
 	public Unit getNovice(String pName) throws RemoteException 
 	{
 		Unit novice = new Novice(pName);
@@ -29,27 +48,40 @@ public class BaseBean extends UnicastRemoteObject implements Base
 		System.out.println("Register updated");
 		return novice;
 	}
-
+	/**
+	 * Prepare the transaction resource with the current transaction (activated by the activeTransaction method).
+	 * @throws RemoteException
+	 */
 	@Override
 	public void prepare() throws RemoteException 
 	{
 		System.out.println("Base resource is now prepared by the transaction "+ currentTransaction.getId());
 	}
-	
+	/**
+	 * Commit the transaction resource with the current transaction (activated by the activeTransaction method).
+	 * @throws RemoteException
+	 */
 	@Override
 	public void commit() throws TransactionException, RemoteException 
 	{
 		System.out.println(String.format("Base resource is now committed (%d operation%s) by the transaction %d ", currentTransaction.getInvocations().size()
 				, (currentTransaction.getInvocations().size() == 1)?"":"s", currentTransaction.getId()));
 	}
-	
+	/**
+	 * Rollback the transaction resource with the current transaction (activated by the activeTransaction method).
+	 * @throws RemoteException
+	 */
 	@Override
 	public void rollback() throws TransactionException, RemoteException 
 	{
 		System.out.println(String.format("Base resource is now rollback (%d operation%s) by the transaction %d ", currentTransaction.getInvocations().size()
 				, (currentTransaction.getInvocations().size() == 1)?"":"s", currentTransaction.getId()));
 	}
-
+	/**
+	 * Active a transaction that will be used by the transaction operations.
+	 * @param pTransaction The transaction that will be used by the transaction operations (prepare, commit, rollback).
+	 * @throws RemoteException
+	 */
 	@Override
 	public void activeTransaction(Transaction pTransaction) throws RemoteException
 	{
