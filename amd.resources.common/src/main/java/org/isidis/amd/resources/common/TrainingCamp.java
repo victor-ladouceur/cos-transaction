a0443de95777fced376f9685c981929e3d02e2d4
steventen <steven@enten.fr>
2013-12-19 16:27:38 +0100

    Javadocumented the amd.resources.common project

--- amd.resources.common/src/main/java/org/isidis/amd/resources/common/TrainingCamp.java
+++ amd.resources.common/src/main/java/org/isidis/amd/resources/common/TrainingCamp.java
@@ -4,7 +4,16 @@ import java.rmi.RemoteException;
 
 import org.isidis.amd.cos.transactions.TransactionResource;
 
+/**
+ * Interface implemented by the concrete training camp resources.
+ */
 public interface TrainingCamp extends TransactionResource 
 {
+	/**
+	 * Returns a soldier unit.
+	 * @param pUnit The novice unit who is trained in the training camp.
+	 * @return A soldier unit.
+	 * @throws RemoteException
+	 */
 	public Unit train(Unit pUnit) throws RemoteException;
 }
--- amd.resources.common/src/main/java/org/isidis/amd/resources/common/Unit.java
+++ amd.resources.common/src/main/java/org/isidis/amd/resources/common/Unit.java
@@ -3,13 +3,40 @@ package org.isidis.amd.resources.common;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+/**
+ * Interface implemented by the concrete units.
+ */
 public interface Unit extends Remote 
 {
+	/**
+	 * Title of the novices.
+	 */
 	public static final String NOVICE_TITLE = "Novice";
+	/**
+	 * Title of the soldiers.
+	 */
 	public static final String SOLDIER_TITLE = "Soldier";
+	/**
+	 * Title of the warriors.
+	 */
 	public static final String WARRIOR_TITLE = "Warrior";
 	
+	/**
+	 * Returns the name of the unit.
+	 * @return The name of the unit.
+	 * @throws RemoteException
+	 */
 	public String getName() throws RemoteException;
+	/**
+	 * Returns the title of the unit.
+	 * @return The title of the unit.
+	 * @throws RemoteException
+	 */
 	public String getTitle() throws RemoteException;
+	/**
+	 * Returns a textual presentation of the unit.
+	 * @return A textual presentation of the unit.
+	 * @throws RemoteException
+	 */
 	public String present() throws RemoteException;
 }
